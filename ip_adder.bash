21c7993f1c543c206a6024d8eab83dd5dfe68518
alom4hdi <119131616+alom4hdi@users.noreply.github.com>
2023-06-29 17:41:54 +0330

    Added argv support.

--- ip_adder.bash
+++ ip_adder.bash
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-echo "Please enter the name of the file containing the IPs:"
-read IP_FILE
+if [[ $# -eq 2 ]] # IF ARGV LEN WAS EQUAL TO 2, GET IP_FILE AND GATEWAY_IP FROM ARGV
+then
+    declare IP_FILE="$1"
+    declare GATEWAY_IP="$2"
+else # OTHERWISE GET THEM FROM STDIN
+    echo "Please enter the name of the file containing the IPs:"
+    read IP_FILE
 
-echo "Please enter the gateway IP:"
-read GATEWAY_IP
+    echo "Please enter the gateway IP:"
+    read GATEWAY_IP
+fi
 
 while IFS= read -r line
 do
